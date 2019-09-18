e0aabdfdc229fe4537a84d04a890d2fb775260c9
Marc Towler <marc@marctowler.co.uk>
2019-09-18 14:40:02 +0100

    Fixed win/loss tracking for monsters Added player vs monster win/loss tracking

--- src/Application/Model/MonsterModel.php
+++ src/Application/Model/MonsterModel.php
@@ -31,13 +31,22 @@ class MonsterModel extends Library\BaseModel
         return $this->_output;
     }
 
-    public function update_stats($id, $outcome)
+    public function update_stats($id, $player, $outcome)
     {
         $stmt = ($outcome == "win") ? 
-            $this->_db->prepare("INSERT INTO monster_stats (nid, wins) VALUES(:npc, wins + 1) ON DUPLICATE KEY UPDATE wins = wins + 1") : 
-            $this->_db->prepare("INSERT INTO monster_stats (nid, losses) VALUES(:npc, losses + 1) ON DUPLICATE KEY UPDATE losses = losses + 1");
+            $this->_db->prepare("INSERT INTO npc_fight_stats (nid, win) VALUES(:npc, win = win + 1) ON DUPLICATE KEY UPDATE win = win + 1") : 
+            $this->_db->prepare("INSERT INTO npc_fight_stats (nid, loss) VALUES(:npc, loss = loss + 1) ON DUPLICATE KEY UPDATE loss = loss + 1");
         $stmt->execute([':npc' => $id]);
 
+        //this is reversed due to being from a monster perspective
+        $stmt2 = ($outcome == "win") ?
+            $this->_db->prepare("INSERT INTO player_vs_monster (cid, mid, loss) VALUES(:player, :npc, loss = loss + 1) ON DUPLICATE KEY UPDATE loss = loss + 1") :
+            $this->_db->prepare("INSERT INTO player_vs_monster (cid, mid, win) VALUES(:player, :npc, win = win + 1) ON DUPLICATE KEY UPDATE win = win + 1");
+        $stmt2->execute([
+            ':npc'    => $id,
+            ':player' => $player
+        ]);
+        
         return true;
     }
 }
